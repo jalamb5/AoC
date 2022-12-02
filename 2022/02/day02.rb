5fa9f19f7f3d2caf774b5b23593d50f3f07b9786
Justin Lamb <justinlamb@me.com>
2022-12-02 10:29:49 -0800

    solve for test_input

--- 2022/02/day02.rb
+++ 2022/02/day02.rb
@@ -1,15 +1,44 @@
 # frozen_string_literal: true
 
-f = File.read('test_input.txt')
-input = f.gsub(/[XYZ]/, 'X' => 'A', 'Y' => 'B', 'Z' => 'C')
-
 # Handle parsing of game of Rock, Paper, Scissors
 # A for Rock, B for Paper, and C for Scissors
 class RPS
+  attr_reader :input
+
   # Hash of each choice with an array of [beats, loses]
   RULES = {
     'A' => %w[C B],
     'B' => %w[A C],
     'C' => %w[B A]
   }
+
+  SCORE = {
+    'A' => 1,
+    'B' => 2,
+    'C' => 3
+  }
+
+  def initialize(file)
+    f = File.open(file).readlines
+    @input = f.each { |line| line.gsub!(/[XYZ]/, 'X' => 'A', 'Y' => 'B', 'Z' => 'C') }
+  end
+
+  def play
+    total_score = 0
+    @input.each do |round|
+      elf = round.split[0]
+      player = round.split[1]
+      total_score += if elf == player
+                       SCORE[player] + 3
+                     elsif elf == RULES[player][0]
+                       SCORE[player]
+                     else
+                       SCORE[player] + 6
+                     end
+    end
+    total_score
+  end
 end
+
+game = RPS.new('test_input.txt')
+p game.play

24fcbd39656354133882a8f3ed4c75eb41f93b08
Justin Lamb <justinlamb@me.com>
2022-12-02 11:08:23 -0800

    refactor @total_score and add #part_two1 with case

--- 2022/02/day02.rb
+++ 2022/02/day02.rb
@@ -4,6 +4,7 @@
 # A for Rock, B for Paper, and C for Scissors
 class RPS
   attr_reader :input
+  attr_accessor :total_score
 
   # Hash of each choice with an array of [beats, loses]
   RULES = {
@@ -19,41 +20,56 @@ class RPS
   }
 
   def initialize(file)
+    @total_score = 0
     f = File.open(file).readlines
     # @input = f.each { |line| line.gsub!(/[XYZ]/, 'X' => 'A', 'Y' => 'B', 'Z' => 'C') }
     @input = f
   end
 
   def part_one
-    total_score = 0
     @input.each do |round|
       elf = round.split[0]
       player = round.split[1]
-      total_score += if elf == player
-                       SCORE[player] + 3
-                     elsif elf == RULES[player][0]
-                       SCORE[player] + 6
-                     else
-                       SCORE[player]
-                     end
+      @total_score += if elf == player
+                        SCORE[player] + 3
+                      elsif elf == RULES[player][0]
+                        SCORE[player] + 6
+                      else
+                        SCORE[player]
+                      end
     end
-    total_score
+    @total_score
   end
 
   def part_two
-    total_score = 0
     @input.each do |round|
       elf = round.split[0]
       player = round.split[1]
-      total_score += if player == 'X'
-                       SCORE[RULES[elf][0]]
-                     elsif player == 'Y'
-                       SCORE[elf] + 3
-                     else
-                       SCORE[RULES[elf][1]] + 6
-                     end
+      @total_score += if player == 'X'
+                        SCORE[RULES[elf][0]]
+                      elsif player == 'Y'
+                        SCORE[elf] + 3
+                      else
+                        SCORE[RULES[elf][1]] + 6
+                      end
     end
-    total_score
+    @total_score
+  end
+
+  def part_two1
+    @input.each do |round|
+      elf = round.split[0]
+      player = round.split[1]
+      case player
+      when 'X'
+        @total_score += SCORE[RULES[elf][0]]
+      when 'Y'
+        @total_score += SCORE[elf] + 3
+      when 'Z'
+        @total_score += SCORE[RULES[elf][1]] + 6
+      end
+    end
+    @total_score
   end
 end
 

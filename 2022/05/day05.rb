563e01bc63ed779f35cea355d4d0dc4501659d8b
Justin Lamb <justinlamb@me.com>
2022-12-05 13:48:13 -0800

    part_one completed

--- 2022/05/day05.rb
+++ 2022/05/day05.rb
@@ -7,7 +7,16 @@ class Stacks
   def initialize(file)
     f = File.read(file).split("\n\n")
     @crate_stacks = crate_stack_gen(normalize_stacks(f[0]))
-    @instructions = f[1].split("\n")
+    @instructions = normalize_instructions(f[1].split("\n"))
+  end
+
+  def part_one
+    move_crates
+    top_items = String.new
+    @crate_stacks.each do |stack|
+      top_items << stack[0]
+    end
+    puts "Part One: #{top_items}"
   end
 
   private
@@ -16,16 +25,17 @@ class Stacks
   def normalize_stacks(string)
     stacks = []
     lines = string.split("\n")
-    num_stacks = lines[0].length / 3 # each stack is 3 chars wide
+    num_stacks = lines[-1].strip[-1]
+    spaces = lines[0].length / num_stacks.to_i # each stack is 3 chars wide
     lines.each do |line|
-      stacks << line.chars.each_slice(num_stacks + 1).map(&:join)
+      stacks << line.chars.each_slice(spaces + 1).map(&:join)
     end
     stacks[0..-2] # return only stack values
   end
 
   # Receive a normalized array and generate stacks
   def crate_stack_gen(lines)
-    stacks = [[], [], []]
+    stacks = [[], [], [], [], [], [], [], [], []]
     lines.each do |line|
       line.each_with_index do |value, index|
         stacks[index] << value.strip.gsub(/[\[\]]/, '') unless value.strip == ''
@@ -33,19 +43,33 @@ class Stacks
     end
     stacks
   end
+
+  # Receive array of instruction strings, convert to array of number values [number to move, from crate, to crate]
+  def normalize_instructions(lines)
+    normalized_instructions = []
+    lines.each do |line|
+      normalized_instructions << line.gsub(/[a-z]/, '').split(' ')
+    end
+    normalized_instructions
+  end
+
+  def move_crates
+    @instructions.each do |number, from, to|
+      number = number.to_i
+      from_stack = @crate_stacks[from.to_i - 1]
+      to_stack = @crate_stacks[to.to_i - 1]
+      until number.zero?
+        crate = from_stack.shift
+        to_stack.prepend(crate)
+        number -= 1
+      end
+    end
+  end
 end
 
-test = Stacks.new('test_input.txt')
-p test.crate_stacks
+# test = Stacks.new('test_input.txt')
+# test.part_one
 
-# Array.new(Array.new(lines.length - 1))
-
-# a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# b = [[],[],[]]
-
-# a.each do |line|
-#   line.each_with_index do |value, index|
-#     b[index] << value
-#   end
-# end
-
+answer = Stacks.new('input.txt')
+answer.crate_stacks
+answer.part_one

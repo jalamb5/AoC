f13e0af7aacd811ae358f85be8ebddfa5651dcf0
Justin Lamb <justinlamb@me.com>
2022-12-06 10:50:47 -0800

    part_one complete

---
+++ 2022/06/day06.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Receive a string of chars, find markers
+class CommsDevice
+  attr_accessor :input
+
+  def initialize(file)
+    @input = File.read(file)
+  end
+
+  def part_one
+    i = 0
+    n = i + 4
+    until n == @input.length
+      return n if @input[i...n].split(//).uniq.length == 4
+
+      i += 1
+      n += 1
+    end
+  end
+end
+
+test = CommsDevice.new('test_input.txt')
+test.part_one
+
+answer = CommsDevice.new('input.txt')
+p answer.part_one
